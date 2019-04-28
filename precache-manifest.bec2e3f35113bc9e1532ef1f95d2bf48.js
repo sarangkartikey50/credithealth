a39c798ceeab9e8af00de46d2c0ba9e650902f51
sarang kartikey <sarangkartikey50@gmail.com>
2019-04-28 13:36:25 +0530

    Updates

---
+++ precache-manifest.bec2e3f35113bc9e1532ef1f95d2bf48.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5c77a6aed75cdad9489effd0d5ea411",
+    "url": "/credithealth/static/media/GoogleSans-Regular.b5c77a6a.ttf"
+  },
+  {
+    "revision": "8d57e4014b18edef070d285746485115",
+    "url": "/credithealth/static/media/GoogleSans-Medium.8d57e401.ttf"
+  },
+  {
+    "revision": "4457817ac2b9993c65e81aa05828fe9c",
+    "url": "/credithealth/static/media/GoogleSans-Bold.4457817a.ttf"
+  },
+  {
+    "revision": "063ec18e65eeb8ac6b31",
+    "url": "/credithealth/static/js/runtime~main.063ec18e.js"
+  },
+  {
+    "revision": "2e33587a33ddb36898cb",
+    "url": "/credithealth/static/js/main.2e33587a.chunk.js"
+  },
+  {
+    "revision": "7c4f740c2d4494e956ca",
+    "url": "/credithealth/static/js/1.7c4f740c.chunk.js"
+  },
+  {
+    "revision": "2e33587a33ddb36898cb",
+    "url": "/credithealth/static/css/main.09106cf9.chunk.css"
+  },
+  {
+    "revision": "71bc8d51119644a516186d8867bfa67c",
+    "url": "/credithealth/index.html"
+  }
+];
